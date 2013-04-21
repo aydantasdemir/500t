35cdd152f7ad392018f46d1416d74afa084ac69a
root <root@li198-231.members.linode.com>
2013-04-21 00:12:12 +0000

    no input for collect_static

--- logar/fabfile.py
+++ logar/fabfile.py
@@ -9,5 +9,5 @@ def restart():
         local("ps ax | grep {0} | awk '{{print $1}}' | xargs kill -9".format(GUNICORN_PORT))
     local("git pull")
     local("./manage.py migrate logs")
-    local("./manage.py collectstatic")
+    local("./manage.py collectstatic --noinput")
     local("python manage.py run_gunicorn -b 0.0.0.0:{0} -D".format(GUNICORN_PORT))
