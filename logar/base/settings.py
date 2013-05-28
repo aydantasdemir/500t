d8f5564e16815be655e076e693bd2086ea2da062
Berker Peksag <berker.peksag@gmail.com>
2013-05-28 09:50:21 +0300

    Cleanup settings.py.

--- logar/base/settings.py
+++ logar/base/settings.py
@@ -11,17 +11,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 ALLOWED_HOSTS = ['500t.org', 'localhost',]
 
 # Local time zone for this installation. Choices can be found here:
@@ -145,23 +134,12 @@ LOGGING = {
     }
 }
 
-
-# django debug toolbar
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-
 MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'pagination.middleware.PaginationMiddleware',
 )
 
 INTERNAL_IPS = ('127.0.0.1',)
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-    'ENABLE_STACKTRACES': True,
-}
 
 TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
                                "django.core.context_processors.debug",
@@ -193,6 +171,11 @@ TEMPLATE_CONTEXT_PROCESSORS += (
     'base.context_processors.user_avatar',
 )
 
+try:
+    from local_settings import *
+except ImportError:
+    class ImproperlyConfigured(Exception):
+        pass
 
-
-from local_settings import *
+    msg = 'move local_settings.py.dist to local_settings.py and set your config.'
+    raise ImproperlyConfigured(msg)
